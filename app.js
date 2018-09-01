1d620979d29bdf23fbfb0c0c96296e9bac378a93
Unknown <jaewellis@gmail.com>
2018-08-31 21:26:13 -0500

    3,2,1 code
    This file contains my work for 321 code.

---
+++ app.js
@@ -0,0 +1,25 @@
+//Create a program that displays your name and age
+let Name = "Jae";
+let age = "24";
+console.log("My name is " + Name + " and I am "+ age + ".");
+
+
+
+//The 6 and 3 Calculator
+let x = 6;
+let y = 3;
+console.log("6+3 is: " + (6+3));
+console.log("6-3 is: " + (6-3));
+console.log("6*3 is: " + (6*3));
+console.log("6/3 is: " + (6/3));
+
+//Observe out put
+
+//Outputs 9
+console.log(4 + 5);
+
+//Outputs "4 + 5"
+console.log("4 + 5");
+
+//Outputs 45
+console.log("4" + "5");
